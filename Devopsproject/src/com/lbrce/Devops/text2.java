5db01107cd396c11efbf7518b3f67ce2780feb7d
AdmiN <AdmiN@ITWS03>
2023-08-10 15:28:38 +0530

    to create devops additional data

---
+++ Devopsproject/src/com/lbrce/Devops/text2.java
@@ -0,0 +1,10 @@
+package com.lbrce.Devops;
+
+public class text2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("devops additional data");
+	}
+
+}
